732fd4e0d119321383782dd23fdde3a130d07351
Vogeel <vitorvogel12@gmail.com>
2022-03-08 15:35:00 -0300

    Add project files.

---
+++ OrientacaoObjeto5.ConsoleApp/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoObjeto5.ConsoleApp
+{
+    internal class Esfera
+    {
+        public double raio;
+        public double volume()
+        {
+            double ValorFinal = 4 * 3.14 * raio * raio* raio / 3;
+            return ValorFinal;
+        }
+    }
+}
---
+++ OrientacaoObjeto5.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OrientacaoObjeto5.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Esfera CalculoEsfera = new Esfera();
+            CalculoEsfera.raio = 3.5;
+            Console.WriteLine($"A esfera tem o volume total de: {CalculoEsfera.volume():F2} metros cúbicos");
+            Console.ReadKey();
+        }
+    }
+}
